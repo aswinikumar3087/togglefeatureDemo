7f911239e16e6742b8857ddb88858b52425a79b4
Kumar parida <aswini.kumarparida@philips.com>
2020-04-03 13:09:55 +0530

    Intial commit

---
+++ feature-toggle-config-server/src/test/java/com/philips/featuretoggleconfigserver/FeatureToggleConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philips.featuretoggleconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureToggleConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
